1a0b45aca071b9307ecadd90f367ad9d9367f5bd
Andres Hernandez <andres.hernandez@utexas.edu>
2020-11-04 19:01:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,7 @@ def key_for_min_value(name_hash)
   if name_hash.empty?
     min_value_key = nil
   else
+    min_value_key = name_hash[0]
     name_hash.each do |key, value|
       if value < min_value_key
         min_value_key = key
