29f108e2f28a8d8df530daf29f2064a0caf78761
Andres Hernandez <andres.hernandez@utexas.edu>
2020-11-04 19:00:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_value_key = 
-  if name_hash.is_empty?
+  min_value_key =
+  if name_hash.empty?
     min_value_key = nil
   else
     name_hash.each do |key, value|
