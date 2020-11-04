b01c284839e16ae59627f56aa505a5a655895270
Andres Hernandez <andres.hernandez@utexas.edu>
2020-11-04 19:21:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-hash = {:blake => 10, :ashley => 50, :adam => 17}
-
 def key_for_min_value(name_hash)
   count = 0
   if name_hash.empty?
